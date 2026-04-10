81d3d5eb095015a953b487d598ab1b20a8d9a75a
chrischiii21 <christymontejo2003@gmail.com>
2026-04-10 13:39:39 +0800

    structured header style

---
+++ tailwind.config.mjs
@@ -0,0 +1,33 @@
+﻿/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  theme: {
+    extend: {
+      colors: {
+        text: '#0f0302',
+        background: '#ffffff',
+        primary: '#1E2023',
+        secondary: '#2B2D30',
+        accent: '#D52212',
+      },
+      fontSize: {
+        sm: '0.750rem',
+        base: '1rem',
+        xl: '1.333rem',
+        '2xl': '1.777rem',
+        '3xl': '2.369rem',
+        '4xl': '3.158rem',
+        '5xl': '4.210rem',
+      },
+      fontFamily: {
+        heading: ['Poppins', 'sans-serif'],
+        body: ['Poppins', 'sans-serif'],
+      },
+      fontWeight: {
+        normal: '400',
+        bold: '700',
+      },
+    },
+  },
+  plugins: [],
+}
